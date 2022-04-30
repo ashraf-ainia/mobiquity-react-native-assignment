263ca324c3d4e49cd2058af7de9d051fd95f2da6
Ashraf Ainia <ashrafainia1995@gmail.com>
2022-04-30 08:27:48 +0300

    feat: config file contains the app base configurations

---
+++ config/app.js
@@ -0,0 +1,4 @@
+export const FLICKER_API_BASE_URL = 'https://api.flickr.com/services/rest';
+
+// In a real app, API keys should be stored in .env file
+export const FLICKER_API_KEY = '11c40ef31e4961acf4f98c8ff4e945d7';
