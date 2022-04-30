3a9e6ea177a032333cfa153ea3d83b3f9dd7446b
Ashraf Ainia <ashrafainia1995@gmail.com>
2022-04-30 08:28:43 +0300

    feat: the app colors as constant so it can be used as theme colors

---
+++ constants/colors.js
@@ -0,0 +1,10 @@
+export default {
+    primaryLight: '#55E6DD',
+    primary: '#44C3C9',
+    gray1: '#EFEFEF',
+    gray2: '#EEE',
+    gray3: '#CCC',
+    gray4: '#AAA',
+    gray5: '#999',
+    gray6: '#888',
+};
