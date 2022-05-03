d74a4bc5d545216b6173326670663a2581430e07
Ashraf Ainia <ashrafainia1995@gmail.com>
2022-05-03 18:59:08 +0300

    style: add TermsSlider title styles

--- components/TermsSlider/styles.js
+++ components/TermsSlider/styles.js
@@ -3,5 +3,9 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
     root: {
         marginVertical: 16
+    },
+    titleText: {
+        marginHorizontal: 8,
+        marginTop: 8
     }
 });
